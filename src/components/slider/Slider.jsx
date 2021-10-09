89288b19f266fb7e195d1a2d31d4ce4ace88c5c7
Milad Amiri <miladmarketing282@gmial.com>
2021-10-09 14:12:06 +0800

    new

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -17,7 +17,7 @@ function SliderEffect() {
         <div className="container">
         <Slider  ref={slider} {...settings}>
             <div className="slide">
-                <img src="https://www3.hilton.com/resources/media/hi/BDOHIHI/en_US/img/shared/full_page_image_gallery/main/HL_purnabreakfast_7_675x359_FitToBoxSmallDimension_Center.jpg" alt="" />
+                <img src="/images/slide1.jpg" alt="" />
                 <h1>Serving Best Restaurant, Catering & Banquet Services in Makassar</h1>
             </div>
             <div className="slide">
@@ -34,8 +34,8 @@ function SliderEffect() {
             </div>
         </Slider>
             <div className="arrows">
-                <span onClick={() => slider?.current?.slickPrev()} className="left"><i class="fas fa-chevron-left"></i></span>
-                <span onClick={() => slider?.current?.slickNext()} className="right"><i class="fas fa-chevron-right"></i></span>
+                <span onClick={() => slider?.current?.slickPrev()} className="left"><i className="fas fa-chevron-left"></i></span>
+                <span onClick={() => slider?.current?.slickNext()} className="right"><i className="fas fa-chevron-right"></i></span>
             </div>
         </div>
     )
